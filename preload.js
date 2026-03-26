386ec6a23a5d0a9d090f10068d3f62463e4f046d
Navaneeth Penumarthi <navaneeth.penumarthi@gmail.com>
2026-03-26 15:16:47 +0530

    Side Note first commit

---
+++ preload.js
@@ -0,0 +1,23 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    // Window control
+    expand: () => ipcRenderer.send('expand'),
+    collapse: () => ipcRenderer.send('collapse'),
+    toggle: () => ipcRenderer.send('toggle'),
+    getExpandedState: () => ipcRenderer.invoke('get-expanded-state'),
+    onExpansionState: (callback) => {
+        ipcRenderer.on('expansion-state', (event, state) => callback(state));
+    },
+
+    // Persistent storage (via main process)
+    storeGet: (key) => ipcRenderer.invoke('store-get', key),
+    storeSet: (key, value) => ipcRenderer.invoke('store-set', key, value),
+
+    // File dialogs
+    showOpenDialog: (options) => ipcRenderer.invoke('show-open-dialog', options),
+    showSaveDialog: (options) => ipcRenderer.invoke('show-save-dialog', options),
+    readFile: (filePath) => ipcRenderer.invoke('read-file', filePath),
+    writeFile: (filePath, content) => ipcRenderer.invoke('write-file', filePath, content),
+    resizeWindow: (width) => ipcRenderer.send('resize-window', width)
+});
